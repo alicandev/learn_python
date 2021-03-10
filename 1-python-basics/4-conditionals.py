56344b5c7e49ef43c7b8d3447153caa1d71c02cc
Alican Demirtas <demirtasdev@hotmail.com>
2021-03-10 05:17:08 +0000

    Conditionals

---
+++ 1-python-basics/4-conditionals.py
@@ -0,0 +1,16 @@
+x, y = 100, 100
+
+# conditional flow
+if (x < y):
+  st = 'x is less than y'
+elif (x > y):
+  st = 'x is greater than y'
+else:
+  st = 'x equals y'
+
+print(st)
+
+# conditional statements
+st = 'x is less than y' if (x < y) else 'x is greater than or equal to y'
+
+print(st)
